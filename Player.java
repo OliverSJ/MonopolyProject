6b93b2e655f2e8301a7a31bce5421f346a4cc94f
Oliver San Juan <osanju2@gmail.com>
2015-10-06 20:00:32 -0500

    Initial commit

---
+++ Player.java
@@ -0,0 +1,10 @@
+// Programmer:  Oliver San Juan
+// Assignment:  Monopoly Project
+// Date:        October 6, 2015
+// Description: This file will create an instance of the Player object.
+//
+
+public class Player
+{
+
+}
