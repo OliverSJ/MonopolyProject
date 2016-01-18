f99ed6ac8579df786e6675b8b6e2be0391e70bfd
Oliver San Juan <osanju2@gmail.com>
2016-01-18 13:34:36 -0600

    Adding final files that were submitted

--- Board.java
+++ Board.java
@@ -1,6 +1,3 @@
-//Programmer: Shagiya Mansuri
-//Board Class
-
 public abstract class Board 
 {
     protected final int address;                     //Address of board location
@@ -17,17 +14,34 @@ public abstract class Board
     //PRE:  address is initialized and address >=0
     //      name is initialized
     //POST: A Board object is created with class member address set to address and class 
-    //      member name set to name.
+    //        member name set to name.
     {
-    	this.address = address;
-    	this.name = name;
+     this.address = address;
+     this.name = name;
     }
-
-    public abstract boolean payRent(Player p);	     //Abstract methos for paying rent
- 
+                 
+    public abstract boolean payRent(Player p, int diceVal, int numOwned);
+    //PRE:  p is initialized
+    //      diceVal >= 1 and diceVal <= 12
+    //      numOwned >= 0
+    //POST: FCTVAL == true if player successfully paid rent, false otherwise
+    
+    
+    public abstract String[] getPossibleActions(Player player); 
+    //PRE: p is initialized
+    //POST: FCTVAL == array of options player has upon landing on
+    //      this space, to be used in a menu in a user interface
+    
     public String toString()
+    //POST: FCTVAL == string representation of board
     {
-    	return "Location name: " + name + ". Address: " + address + ". ";
+        return "Location name: " + name + ". Address: " + address + ". ";
     }
-}
+    
+    public int getAddress()
+    //POST: FCTVAL == private data member address
+    {
+        return address;
+    }
 
+}
--- Player.java
+++ Player.java
@@ -1,8 +1,3 @@
-// Programmer:  Oliver San Juan
-// Assignment:  Monopoly Project
-// Date:        October 10, 2015
-// Description: This file will create an instance of the Player object.
-//
 
 public class Player
 {
@@ -12,28 +7,34 @@ public class Player
     private int wealth;                                         //The amount of money a player has
     private int playerPosition;                                 //The player's position on the board
     private String token;                                       //The player's token
-    private boolean[] properties;                               //The player's properties, array
-                                                                // is indexed via the board positions
+    private Board[] properties;                               //The player's properties, array
+                                                                // is indexed via propertiesIndex
+    private int propertiesIndex;
     private String stringVal;                                   //String representation of all of 
                                                                 //  the player's properties
     private int numOfRailroadsOwned;                            //The number of railroads the player
                                                                 //  owns
     private int numOfUtilitiesOwned;                            //The number of utilities the player
                                                                 //  owns
-
+    
+    private int numOfHotels;                                    //The amount of hotels that the player
+                                                                //  owns
+    private int numOfHouses;                                    //The amount of hotels that the player
+                                                                //  owns
 
     public Player()
     //POST: A default Player object is contructed with moneyAmt set to 1500 (the starting amount of
     //      money for each player in the game), playerPosition set to 0, token set to 0, and 
     //      allocating the properties array
     {
-        wealth = 1500;
+        wealth = 1;
         playerPosition = 0;
         token = " ";
-        properties = new boolean[NUM_OF_BOARD_POSITIONS];
+        properties = new Board[NUM_OF_BOARD_POSITIONS];
         stringVal = " ";
         numOfUtilitiesOwned = 0;
         numOfRailroadsOwned = 0;
+        propertiesIndex = 0;
 
     }
 
@@ -41,7 +42,7 @@ public class Player
     //PRE:  token >=0
     //POST: A Player object is constructed with private class member token being set to token
     {
-        super();
+        this();
         this.token = token;
     }
 
@@ -50,6 +51,11 @@ public class Player
     //POST: The Player's position is updated by moving it ahead by the sum of the dice values
     {
         playerPosition += diceValue;
+        
+        if(playerPosition >= 40)
+        {
+            playerPosition -= 40;
+        }
     }
 
     public boolean changeWealth(int changeValue)
@@ -62,23 +68,24 @@ public class Player
         return (wealth >= 0) ? true : false;
     }
 
-    public boolean buyProperty(int cost, int propertyBoardPosition)
+    public boolean buyProperty(int cost, int propertyBoardPosition, Board[] properties)
     //PRE:  cost > 0 && propertyBoardPosition > 0 && propertyBoardPosition <= 40 && the property on 
     //      propertyBoardPosition is not owned by another player
     //POST: properties is updated to indicate that the player owns the property on the board position
     //      FCTVAL == true if the purchase was successful, false if it was not
     {
-        if((wealth - cost) > 0)         //If the person can afford it
-        {   
+        if((wealth - cost) > 0)
+        {
             wealth -= cost;
-            properties[propertyBoardPosition] = true;
+            this.properties[propertiesIndex] = ((Property)(properties[propertyBoardPosition]));
+            propertiesIndex++;
             return true;
         }
-
         else
         {
             return false;
         }
+
     }
 
     public void addRailRoad()
@@ -93,44 +100,56 @@ public class Player
         numOfUtilitiesOwned++;
     }
 
-    public String getProperties(Property[] properties)
+    public Board[] getPropertiesArray()
+    {
+        return properties;
+    }
+
+    public String getProperties()
     //PRE:  properties is initialized
-    //POST: FCTVAL == string representation of all of the Player's properties
+    //POST: FCTVAL == string representation of all of the Player's properties and the address
+    //      of the property on the board
     {               
         stringVal = " ";                  //Clear out stringVal from previous calls to getProperties
-
-        //Iterator through all of the private class member array properties.  If any value is true,
-        // use that index to look up the properties' name
-        for(int i = 0; i < NUM_OF_BOARD_POSITIONS; ++i)
+        
+        //Iterate through the private properties array, printing out all of the property names
+        for(int i = 0; i < propertiesIndex; i++)
         {
-            if(this.properties[i] == true && stringVal == " ")
-            {
-                stringVal += properties[i].name;
+            if(stringVal == " ")                    //If the stringVal is empty, then we haven't stored any
+            {                                       //  properties yet
+                stringVal += properties[i].name + "[" + properties[i].address + "]";
             }
 
-            else if(this.properties[i] == true)      //If the player owns that property, then add it to             
-            {                                        //  to stringVal
+            else                                    //If there are any additional properties to be printed out,
+            {                                       //  then prepend the comma
 
-                stringVal += ", " + properties[i].name;
+                stringVal += ", " + properties[i].name + "[" + properties[i].address + "]";
             }
-
         }
 
         return stringVal;
     }
 
-    public String toString(Property[] properties)
+    public String toString()
     //PRE: properties is initialized
     //POST: FCTVAL == string representation of all of the Player object's attributes
     {
-        stringVal = getProperties(properties);
+        stringVal = getProperties();
         return "Player's Token:" + token + "\n" + "Player's Wealth: " + wealth + "\n" + 
                 "Player's position: " + playerPosition + "\n" + 
-                "Number of Railroads this player owns: " + numOfRailroadsOwned +
-                "Number of utilities this player owns: " + numOfUtilitiesOwned + 
+                "Number of Railroads this player owns: " + numOfRailroadsOwned + "\n" +
+                "Number of utilities this player owns: " + numOfUtilitiesOwned + "\n" +
+                "Number of houses this player owns: " + numOfHouses + "\n" +
+                "Number of hotels this player owns: " + numOfHotels + "\n" +
                 "Properties owned by player: " + stringVal + "\n";
 
-    }  
+    }
+
+    public String getWealthAndPosition()
+    //POST: FCTVAL == string representation of private class members toke, playerPosition, and wealth
+    {
+        return "Player: " + token + " Location: " + playerPosition + " Wealth: " + wealth + "\n";
+    }
 
     public int getWealth()
     //POST: FCTVAL == private class member wealth
@@ -139,7 +158,7 @@ public class Player
     }
 
     public int getPlayerPosition()
-    /POST:  FCTVAL == private class member playerPosition
+    //POST:  FCTVAL == private class member playerPosition
     {
         return playerPosition;
     }
@@ -162,5 +181,52 @@ public class Player
         return numOfUtilitiesOwned;
     }
 
+    public int getNumOfHouses() 
+    //POST: FCTVAL == number of houses player owns
+    {
+        return numOfHouses;
+    }
 
+    public void setNumOfHouses(int numOfHouses)
+    //PRE: numOfHouses >= 0
+    //POST: Sets class member numOfHouses to numofHouses
+    {
+        this.numOfHouses = numOfHouses;
+    }
+    public int getNumOfHotels() 
+    //POST: FCTVAL == number of hotels player owns
+    {
+        return numOfHotels;
+    }
+
+    public void setNumOfHotels(int numOfHotels)
+    //PRE: numOfHotels >= 0
+    //POST: Sets class member numOfHotels to numofHotels
+    {
+        this.numOfHotels = numOfHotels;
+    }
+
+    public int getPropertiesIndex()
+    //POST: FCTVAL == private data member 
+    {
+        return propertiesIndex;
+    }
+
+    public boolean hotelCheck()
+    //POST: FCTVAL == Returns true if any player owned properties has 4 houses else false 
+    {
+        int trav = 0;                   //used to traverse through player owned propeties 
+        
+        while(trav < propertiesIndex)   //traverse till number of properties 
+        {
+      	  if(properties[trav] instanceof Lot)
+      	  {
+      		  if(((Lot) properties[trav]).getNumHousesOnLot() ==4) 
+      			  return true;
+      	  }
+      	  trav++;
+        }
+        
+        return false;                   //none of the player owned properties have 4 houses 
+    }
 }
